6d1e02269ce96f915f5765b817e242a20f2fea14
Muhammed Sahad P <zanusahad@gmail.com>
2025-02-18 16:57:12 +0530

    feat(server):setup errorCatch and error Response

---
+++ server/src/utils/standardResponse.ts
@@ -0,0 +1,14 @@
+export class StandardResponse {
+    // all responses will extend this class
+    statusCode: number;
+    status: string;
+    message: string;
+    data?: any;
+    constructor(message: string, data?: any, statusCode = 200) {
+      this.statusCode = statusCode;
+      this.status = "success";
+      this.message = message;
+      this.data = data;
+    }
+  }
+  
